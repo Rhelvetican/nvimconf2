105faf2bf91e97809445638f4ae8e88d1c3f7692
Rh4096 <rhelvetican@gmail.com>
2025-02-20 04:34:46 +0700

    adds patterns

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,6 +1,17 @@
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_configs.lua_patterns = {
+	install_info = {
+		url = "https://github.com/OXY2DEV/tree-sitter-lua_patterns",
+		files = { "src/parser.c" },
+		branch = "main",
+	},
+}
+
 require("nvim-treesitter.configs").setup({
 	modules = {},
 	sync_install = true,
+	auto_install = true,
 
 	ignore_install = {},
 	ensure_installed = {
