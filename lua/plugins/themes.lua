608d5ee9eafd285a4d1e85eef3a3d49828f50b1e
Rh4096 <rhelvetican@gmail.com>
2025-02-20 22:42:07 +0700

    tweak

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -36,12 +36,17 @@ return {
 			require("catppuccin").setup({
 				styles = {
 					comments = { "italic" },
-					keywords = { "italic" },
-					variables = { "italic", "bold" },
-					types = { "bold" },
+					conditionals = { "italic" },
+					loops = { "italic", "bold" },
 					functions = { "italic", "bold" },
-					miscs = { "italic" },
-					properties = { "italic" },
+					keywords = { "italic", "bold" },
+					variables = { "italic", "bold" },
+					strings = { "bold" },
+					numbers = { "bold" },
+					booleans = { "bold" },
+					properties = { "italic", "bold" },
+					types = { "italic", "bold" },
+					operators = { "bold" },
 				},
 
 				integrations = {
@@ -49,7 +54,6 @@ return {
 					cmp = true,
 					dap = true,
 					mason = true,
-					mini = {},
 					dap_ui = true,
 					aerial = true,
 					snacks = true,
@@ -60,45 +64,19 @@ return {
 					treesitter_context = true,
 				},
 
+				color_overrides = {
+					all = {
+						base = "#0E0E1E",
+						mantle = "#0E0E1E",
+						crust = "#0E0E1E",
+					},
+				},
+
 				custom_highlights = function(color)
 					return {
 						Special = { fg = color.overlay2 },
 					}
 				end,
-			})
-		end,
-	},
-
-	{
-		"nuvic/flexoki-nvim",
-		name = "flextoki",
-		lazy = false,
-		priority = 9999,
-		config = function()
-			local palette = require("flexoki.palette")
-			require("flexoki").setup({
-				---@type table<string, Highlight?>
-				highlight_groups = {
-					Boolean = { fg = palette.magenta_two, bold = true, italic = true },
-					Comment = { fg = palette.subtle, italic = true },
-					Constant = { fg = palette.yellow_two, bold = true },
-					Error = { fg = palette.red_two, bold = true },
-					Exception = { fg = palette.blue_two, italic = true },
-					Function = { fg = palette.orange_two, bold = true, italic = true },
-					Keyword = { fg = palette.green_two, bold = true },
-					Label = { fg = palette.cyan_two, bold = true },
-					Macro = { fg = palette.purple_two, bold = true, italic = true },
-					Number = { fg = palette.purple_two, bold = true, italic = true },
-					Operator = { fg = palette.subtle, bold = true },
-					String = { fg = palette.cyan_two, bold = true },
-					Type = { fg = palette.cyan_two, bold = true, italic = true },
-
-					["@keyword.repeat"] = { fg = palette.green_two, bold = true },
-					["@keyword.return"] = { fg = palette.green_two, bold = true },
-					["@keyword.exception"] = { fg = palette.green_two, bold = true },
-					["@keyword.conditional"] = { fg = palette.green_two, bold = true },
-					["@keyword.conditional.ternary"] = { fg = palette.green_two, bold = true },
-				},
 			})
 		end,
 	},
