9302a0848cc30e11018931e931355ea3fcc596f8
Rh4096 <rhelvetican@gmail.com>
2025-04-16 15:45:18 +0700

    new keymappings

--- lua/keys/snacks.lua
+++ lua/keys/snacks.lua
@@ -1,261 +1,76 @@
 ---@module "snacks"
 
----@class SnacksKeymap
----@field desc string
----@field [1] string
----@field [2] function
-
 ---@param keymap string
 ---@param func function
----@param desc string
----@return SnacksKeymap
-local function make_snacks_mapping(keymap, func, desc)
-	return {
-		keymap,
-		func,
-		desc = desc,
-	}
+---@param opts table
+---@return table
+local function make_snacks_mapping(keymap, func, opts)
+	return vim.tbl_deep_extend("force", { [1] = keymap, [2] = func }, opts)
 end
 
 return {
 	make_snacks_mapping("<space>b", function()
 		Snacks.picker.buffers()
-	end, "Pick buffers"),
+	end, { desc = "Pick buffers" }),
 	make_snacks_mapping("<space>gg", function()
 		Snacks.picker.grep()
-	end, "Grep"),
+	end, { desc = "Grep" }),
 	make_snacks_mapping("<space>ch", function()
 		Snacks.picker.command_history()
-	end, "Command History"),
-	{
-		"<space>f",
-		function()
-			Snacks.picker.files()
-		end,
-		desc = "Find Files",
-	},
+	end, { desc = "Command History" }),
+	make_snacks_mapping("<space>f", function()
+		Snacks.picker.files()
+	end, { desc = "Find Files" }),
 	-- find
-	{
-		"<space>cc",
-		function()
-			Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
-		end,
-		desc = "Find Config File",
-	},
-	{
-		"<space>gf",
-		function()
-			Snacks.picker.git_files()
-		end,
-		desc = "Find Git Files",
-	},
-	{
-		"<space>fr",
-		function()
-			Snacks.picker.recent()
-		end,
-		desc = "Recent",
-	},
+	make_snacks_mapping("<space>cc", function()
+		Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+	end, { desc = "Find Config File" }),
+	make_snacks_mapping("<space>gf", function()
+		Snacks.picker.git_files()
+	end, { desc = "Find Git Files" }),
+	make_snacks_mapping("<space>fr", function()
+		Snacks.picker.recent()
+	end, { desc = "Recent" }),
 	-- git
-	{
-		"<space>gl",
-		function()
-			Snacks.picker.git_log()
-		end,
-		desc = "Git Log",
-	},
-	{
-		"<leader>pgs",
-		function()
-			Snacks.picker.git_status()
-		end,
-		desc = "Git Status",
-	},
-	{
-		"<leader>pgd",
-		function()
-			Snacks.picker.git_diff()
-		end,
-		desc = "Git Diff",
-	},
+	make_snacks_mapping("<space>gl", function()
+		Snacks.picker.git_log()
+	end, { desc = "Git Log" }),
+	make_snacks_mapping("<leader>pgs", function()
+		Snacks.picker.git_status()
+	end, { desc = "Git Status" }),
+	make_snacks_mapping("<leader>pgd", function()
+		Snacks.picker.git_diff()
+	end, { desc = "Git Diff" }),
 	-- Grep
-	{
-		"<leader>pgl",
-		function()
-			Snacks.picker.lines()
-		end,
-		desc = "Buffer Lines",
-	},
-	{
-		"<leader>pgb",
-		function()
-			Snacks.picker.grep_buffers()
-		end,
-		desc = "Grep Open Buffers",
-	},
-	{
-		"<leader>pgg",
-		function()
-			Snacks.picker.grep()
-		end,
-		desc = "Grep",
-	},
-	{
-		"<leader>pgw",
-		function()
-			Snacks.picker.grep_word()
-		end,
-		desc = "Visual selection or word",
-		mode = { "n", "x" },
-	},
-	-- search
-	{
-		"<leader>psr",
-		function()
-			Snacks.picker.registers()
-		end,
-		desc = "Registers",
-	},
-	{
-		"<leader>psa",
-		function()
-			Snacks.picker.autocmds()
-		end,
-		desc = "Autocmds",
-	},
-	{
-		"<leader>psC",
-		function()
-			Snacks.picker.command_history()
-		end,
-		desc = "Command History",
-	},
-	{
-		"<leader>psc",
-		function()
-			Snacks.picker.commands()
-		end,
-		desc = "Commands",
-	},
-	{
-		"<leader>psd",
-		function()
-			Snacks.picker.diagnostics()
-		end,
-		desc = "Diagnostics",
-	},
-	{
-		"<leader>psh",
-		function()
-			Snacks.picker.help()
-		end,
-		desc = "Help Pages",
-	},
-	{
-		"<leader>psH",
-		function()
-			Snacks.picker.highlights()
-		end,
-		desc = "Highlights",
-	},
-	{
-		"<leader>psj",
-		function()
-			Snacks.picker.jumps()
-		end,
-		desc = "Jumps",
-	},
-	{
-		"<leader>psk",
-		function()
-			Snacks.picker.keymaps()
-		end,
-		desc = "Keymaps",
-	},
-	{
-		"<leader>psl",
-		function()
-			Snacks.picker.loclist()
-		end,
-		desc = "Location List",
-	},
-	{
-		"<leader>psM",
-		function()
-			Snacks.picker.man()
-		end,
-		desc = "Man Pages",
-	},
-	{
-		"<leader>psm",
-		function()
-			Snacks.picker.marks()
-		end,
-		desc = "Marks",
-	},
-	{
-		"<leader>psR",
-		function()
-			Snacks.picker.resume()
-		end,
-		desc = "Resume",
-	},
-	{
-		"<leader>psq",
-		function()
-			Snacks.picker.qflist()
-		end,
-		desc = "Quickfix List",
-	},
-	{
-		"<leader>pqp",
-		function()
-			Snacks.picker.projects()
-		end,
-		desc = "Projects",
-	},
+	make_snacks_mapping("<leader>pgl", function()
+		Snacks.picker.lines()
+	end, { desc = "Buffer Lines" }),
+	make_snacks_mapping("<leader>pgb", function()
+		Snacks.picker.grep_buffers()
+	end, { desc = "Grep Open Buffers" }),
+	make_snacks_mapping("<leader>pgg", function()
+		Snacks.picker.grep()
+	end, { desc = "Grep" }),
+	make_snacks_mapping("<leader>pgw", function()
+		Snacks.picker.grep_word()
+	end, { desc = "Visual selection or word", mode = { "n", "x" } }),
 	-- LSP
-	{
-		"<leader>pgd",
-		function()
-			Snacks.picker.lsp_definitions()
-		end,
-		desc = "Goto Definition",
-	},
-	{
-		"<leader>pgr",
-		function()
-			Snacks.picker.lsp_references()
-		end,
-		nowait = true,
-		desc = "References",
-	},
-	{
-		"<leader>pgI",
-		function()
-			Snacks.picker.lsp_implementations()
-		end,
-		desc = "Goto Implementation",
-	},
-	{
-		"<leader>pgy",
-		function()
-			Snacks.picker.lsp_type_definitions()
-		end,
-		desc = "Goto T[y]pe Definition",
-	},
-	{
-		"<leader>pss",
-		function()
-			Snacks.picker.lsp_symbols()
-		end,
-		desc = "LSP Symbols",
-	},
-	{
-		"<leader>cs",
-		function()
-			Snacks.picker.colorschemes()
-		end,
-		desc = "Choosing colorschemes.",
-	},
+	make_snacks_mapping("ld", function()
+		Snacks.picker.lsp_definitions()
+	end, { desc = "Goto Definition" }),
+	make_snacks_mapping("lr", function()
+		Snacks.picker.lsp_references()
+	end, { nowait = true, desc = "References" }),
+	make_snacks_mapping("lI", function()
+		Snacks.picker.lsp_implementations()
+	end, { desc = "Goto Implementation" }),
+	make_snacks_mapping("ltd", function()
+		Snacks.picker.lsp_type_definitions()
+	end, { desc = "Goto T[y]pe Definition" }),
+	make_snacks_mapping("ls", function()
+		Snacks.picker.lsp_symbols()
+	end, { desc = "LSP Symbols" }),
+	make_snacks_mapping("<leader>cs", function()
+		Snacks.picker.colorschemes()
+	end, { desc = "Choosing colorschemes." }),
 }
