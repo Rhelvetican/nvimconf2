1e2602eb784ca37904879afedfe291c7206aa7c8
Rh4096 <rhelvetican@gmail.com>
2025-06-12 20:09:41 +0700

    smods

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -9,13 +9,18 @@ return {
 		opts = {
 			library = {
 				"lazy.nvim",
+
 				{ path = "${3rd}/luv/library" },
+
 				{ plugins = "nvim-dap-ui", types = true },
 
 				{
 					path = "~/AppData/Local/nvim-data/mason/packages/lua-language-server/LLS-Addons/addons/xmake/module/library/",
 					files = { "xmake.lua" },
 				},
+
+				{ path = "${3rd}/smods/library" },
+				{ path = "${3rd}/love2d/library" },
 			},
 		},
 	},
