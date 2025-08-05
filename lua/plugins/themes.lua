177131fd00a2b115fa8066cd8afcfd49d571dae4
Rh4096 <rhelvetican@gmail.com>
2025-08-05 18:50:02 +0700

    Vtty

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,3 +1,5 @@
+---@module "lazy"
+---@type LazySpec
 return {
 	{
 		"rktjmp/lush.nvim",
@@ -32,23 +34,44 @@ return {
 	},
 
 	{
-		"mcauley-penney/techbase.nvim",
-		priority = 1000,
-	},
-
-	{
 		"hyperb1iss/silkcircuit-nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("silkcircuit").setup({})
-		end,
-	},
+			require("silkcircuit").setup({
+				integrations = {
+					treesitter = true,
+					lsp = true,
+					telescope = true,
+					nvimtree = true,
+					neotree = true,
+					gitsigns = true,
+					indent_blankline = true,
+					bufferline = true,
+					lualine = true,
+					dashboard = true,
+					alpha = true,
+					which_key = true,
+					cmp = true,
+					notify = true,
+					noice = true,
+					hop = true,
+					leap = true,
+					lazy = true,
+					mason = true,
+					nvim_dap = true,
+					trouble = true,
+					outline = true,
+					illuminate = true,
+					flash = true,
+					rainbow_delimiters = true,
+				},
 
-	{
-		"forest-nvim/sequoia.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function() end,
+				on_highlights = function(hl, _)
+					hl.Delimiter = hl["@punctuation.special"]
+					hl.SpecialKey = hl["@punctuation.special"]
+				end,
+			})
+		end,
 	},
 }
