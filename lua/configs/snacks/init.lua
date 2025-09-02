534c4c1e059478dc2b3e50f0f4dc898b872240d4
Rh4096 <rhelvetican@gmail.com>
2025-09-02 22:11:09 +0700

    fmt

--- lua/configs/snacks/init.lua
+++ lua/configs/snacks/init.lua
@@ -1,13 +1,10 @@
----@module "snacks"
----@class snacks.Config
+---@type snacks.Config
 return {
 	bigfile = { enabled = true },
 	dashboard = { enabled = true },
-	explorer = { enabled = true },
 	indent = { enabled = true, animate = { enabled = false }, chunk = { enabled = true } },
 	input = { enabled = true },
 	picker = { enabled = true },
-	quickfile = { enabled = true },
 	statuscolumn = { enabled = true },
 	words = { enabled = true },
 }
