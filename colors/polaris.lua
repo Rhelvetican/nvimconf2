e085327ab3559aa897d94c4d2f1dd478e7436ab0
Rh4096 <rhelvetican@gmail.com>
2025-09-11 02:41:03 +0700

    :/
    Signed-off-by: Rh4096 <rhelvetican@gmail.com>

--- colors/polaris.lua
+++
@@ -1,8 +0,0 @@
-require("xeno").setup({
-  base = "#1E1E1E",
-  accent = "#8CBE8C",
-  variation = 0.6,
-  contrast = 0.8,
-  transparent = false,
-})
-vim.g.colors_name = "polaris"
--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -2,6 +2,50 @@ return {
 	"vyfor/cord.nvim",
 	build = ":Cord update",
 	opts = function()
-		require("configs.cord")
+		---@type {[string]: fun(opts: CordOpts): string}
+		Languages = {}
+
+		Languages["Rust"] = function(o)
+			return "Oxidizing " .. o.filename .. " (Line " .. o.cursor_line .. ")"
+		end
+
+		Languages["Zig"] = function(o)
+			return "Zigging " .. o.filename .. " (Line " .. o.cursor_line .. ")"
+		end
+
+		Languages["Lua"] = function(o)
+			return "Writing (probably) awesome Lua at " .. o.filename .. " (Line " .. o.cursor_line .. ")"
+		end
+
+		Languages["rust"] = Languages["Rust"]
+		Languages["zig"] = Languages["Zig"]
+		Languages["lua"] = Languages["Lua"]
+
+		require("cord").setup({
+			editor = {
+				client = "neovim",
+				tooltip = "The Holy Text Editor",
+			},
+
+			display = {
+				theme = "catppuccin",
+				flavor = "accent",
+			},
+
+			text = {
+				editing = function(o)
+					vim.notify(o.filetype, 1, {})
+					if Languages[o.filetype] ~= nil then
+						return Languages[o.filetype](o)
+					end
+
+					return "Editing " .. o.filename
+				end,
+
+				workspace = function(o)
+					return "Working on " .. o.workspace
+				end,
+			},
+		})
 	end,
 }
