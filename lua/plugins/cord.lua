c93814972f2df1edc9ba8f5934133774cee83d23
Rh4096 <rhelvetican@gmail.com>
2025-09-11 17:24:44 +0700

    :P

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,30 +1,30 @@
+---@type {[string]: fun(opts: CordOpts): string}
+local Languages = {}
+
+---@class Language
+---@field language string|string[]
+---@field message fun(opts: CordOpts): string
+---@overload fun(o: Language): Language
+local Language = setmetatable({}, {
+	---@param o Language
+	---@return Language
+	__call = function(_, o)
+		if type(o.language) == "string" then
+			Languages[o.language] = o.message
+			Languages[o.language:lower()] = o.message
+		else
+			for _, lang in ipairs(o.language) do
+				Languages[lang] = o.message
+			end
+		end
+		return o
+	end,
+})
+
 return {
 	"vyfor/cord.nvim",
 	build = ":Cord update",
 	config = function()
-		---@type {[string]: fun(opts: CordOpts): string}
-		local Languages = {}
-
-		---@class Language
-		---@field language string|string[]
-		---@field message fun(opts: CordOpts): string
-		---@overload fun(o: Language): Language
-		local Language = setmetatable({}, {
-			---@param o Language
-			---@return Language
-			__call = function(_, o)
-				if type(o.language) == "string" then
-					Languages[o.language] = o.message
-					Languages[o.language:lower()] = o.message
-				else
-					for _, lang in ipairs(o.language) do
-						Languages[lang] = o.message
-					end
-				end
-				return o
-			end,
-		})
-
 		---@param s string
 		---@return fun()
 		local function create_message(s)
@@ -38,7 +38,7 @@ return {
 					o.filename,
 					o.cursor_line,
 					(diagnostics > 0)
-							and string.format("(%d %s)", diagnostics, (diagnostics < 2) and "problem" or "problems")
+							and string.format("(%d %s)", diagnostics, (diagnostics < 2) and "Problem" or "Problems")
 						or ""
 				)
 			end
@@ -65,7 +65,7 @@ return {
 		})
 
 		Language({
-			language = { "JavaScript", "TypeScript" },
+			language = { "JavaScript", "TypeScript", "JS", "TS" },
 			message = create_message("This is Hell, and it's at "),
 		})
 
@@ -94,13 +94,26 @@ return {
 					vim.notify(o.filetype, 1, {})
 					if Languages[o.filetype] ~= nil then
 						return Languages[o.filetype](o)
+					else
+						local diagnostics = o.diagnostics(o)
+						return string.format(
+							"Editing %s (Line %d) (%d %s)",
+							o.filename,
+							o.cursor_line,
+							diagnostics,
+							(diagnostics < 2) and "Problem" or "Problems"
+						)
 					end
-
-					return "Editing " .. o.filename .. "(Line" .. o.cursor_line .. ")"
 				end,
 
 				workspace = function(o)
-					return "Working on " .. o.workspace
+					local diagnostics = o.diagnostics(o)
+					return string.format(
+						"Working on %s (%d %s)",
+						o.workspace,
+						diagnostics,
+						(diagnostics < 2) and "Problem" or "Problems"
+					)
 				end,
 			},
 		})
